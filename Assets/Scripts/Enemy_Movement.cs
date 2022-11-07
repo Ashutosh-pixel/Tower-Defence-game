e808158d3a5a59230f078bc3577118c94ea0f793
ashutosh sharma <ashutosh2221998@gmail.com>
2022-11-07 10:12:38 +0530

    Prototype

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> path = new List<Transform>();
+    [SerializeField] [Range(0,5)]float speed = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(Movement());
+    }
+
+    IEnumerator Movement()
+    {
+        foreach (Transform item in path)
+        {
+            Vector3 startposition = transform.position;
+            float elapsedtime = 0;
+            Vector3 endposition = item.transform.position;
+            transform.LookAt(endposition);
+
+            while (elapsedtime < 1f)
+            {
+                elapsedtime += Time.deltaTime*speed;
+                transform.position = Vector3.Lerp(startposition, endposition, elapsedtime);
+                yield return null;
+            }
+        }
+    }
+
+
+}
